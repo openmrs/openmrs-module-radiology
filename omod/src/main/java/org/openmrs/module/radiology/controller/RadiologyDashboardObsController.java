5303209d0561dc55a5bc83e2a825020fa7db70d1
teleivo <mrteleivo@gmail.com>
2015-04-18 17:30:53 +0200

    move web related code to omod

--- omod/src/main/java/org/openmrs/module/radiology/controller/RadiologyDashboardObsController.java
+++ omod/src/main/java/org/openmrs/module/radiology/controller/RadiologyDashboardObsController.java
@@ -7,7 +7,7 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS 
  * graphic logo is a trademark of OpenMRS Inc.
  */
-package org.openmrs.module.radiology.web;
+package org.openmrs.module.radiology.controller;
 
 import java.util.List;
 import java.util.Locale;
