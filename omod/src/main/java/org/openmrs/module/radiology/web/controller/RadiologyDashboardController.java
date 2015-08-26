fcf32c0b579757f7867a92b57831b0a6fa47ea60
bgeVam <georg.bernold@gmx.at>
2015-08-26 22:40:01 +0200

    RAD-66 Refactor RadiologyDashboardController Including Tests

--- omod/src/main/java/org/openmrs/module/radiology/web/controller/RadiologyDashboardController.java
+++ omod/src/main/java/org/openmrs/module/radiology/web/controller/RadiologyDashboardController.java
@@ -9,52 +9,54 @@
  */
 package org.openmrs.module.radiology.web.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Order;
 import org.openmrs.Patient;
 import org.openmrs.api.OrderService;
-import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.radiology.RadiologyService;
 import org.openmrs.module.radiology.Study;
 import org.openmrs.module.radiology.Utils;
 import org.openmrs.module.radiology.web.util.StudyStatusColumnGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-public class RadiologyDashboardController implements Controller {
+@Controller
+public class RadiologyDashboardController {
 	
 	Log log = LogFactory.getLog(getClass());
 	
-	static RadiologyService radiologyService() {
-		return Context.getService(RadiologyService.class);
-	}
+	@Autowired
+	RadiologyService radiologyService;
 	
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-	        IOException {
-		String patientId = request.getParameter("patientId");
-		return ordersTable(patientId);
-	}
+	@Autowired
+	OrderService orderService;
 	
-	ModelAndView ordersTable(String patientId) {
-		String str = "";
-		String route = "/module/radiology/portlets/RadiologyDashboardTab";
-		ModelAndView mav = new ModelAndView(route);
+	/**
+	 * Get all orders for given patient criteria
+	 * 
+	 * @param patient the patient the orders should be returned for
+	 * @return model and view containing table of orders corresponding to given criteria
+	 * @should return model and view populated with all orders given patient
+	 * @should return empty model and view populated no orders given invalid patient
+	 */
+	@RequestMapping("**/RadiologyDashboardTab.portlet")
+	public ModelAndView ordersTable(@RequestParam(value = "patientId", required = true) Patient patient) {
 		
-		List<Order> matchedOrders = getRadiologyOrdersByPatient(patientId, mav);
+		ModelAndView mav = new ModelAndView("/module/radiology/portlets/RadiologyDashboardTab");
+		
+		List<Order> matchedOrders = orderService.getOrders(Order.class, Arrays.asList(patient), null, null, null, null,
+		    Utils.getRadiologyOrderType());
 		// TODO Status filter
-		List<Study> studies = radiologyService().getStudiesByOrders(matchedOrders);
+		List<Study> studies = radiologyService.getStudiesByOrders(matchedOrders);
 		List<String> statuses = new Vector<String>();
 		List<String> priorities = new Vector<String>();
 		List<String> schedulers = new Vector<String>();
@@ -85,26 +87,10 @@ public class RadiologyDashboardController implements Controller {
 		mav.addObject("readings", readings);
 		mav.addObject("modalities", modalities);
 		mav.addObject("matchedOrdersSize", matchedOrders.size());
-		//if(Context.getAuthenticatedUser().hasRole(Roles.ReadingPhysician, true)){
 		mav.addObject("obsId", "&obsId");
-		//}
 		mav.addObject("mwlStatuses", mwlStatuses);
-		mav.addObject("patientId", patientId);
 		
-		log.debug("\n***\n" + str + "\n///");
 		return mav;
-	}
-	
-	private List<Order> getRadiologyOrdersByPatient(String patientId, ModelAndView mav) {
-		
-		OrderService os = Context.getOrderService();
-		PatientService ps = Context.getPatientService();
-		Patient p = ps.getPatient(Integer.valueOf(patientId));
-		List<Patient> plist = new ArrayList();
-		plist.add(p);
-		List<Order> matchedOrders = os.getOrders(Order.class, plist, null, null, null, null, Utils.getRadiologyOrderType());
-		
-		return matchedOrders;
 	}
 	
 }
