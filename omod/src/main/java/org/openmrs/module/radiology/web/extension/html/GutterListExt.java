ed02ec9fb5c28791c0854d9c0655200d1bb71907
Ivange Larry <ivangelarry@gmail.com>
2016-05-16 11:03:22 +0100

    RAD-247: Create radiology menu entry

---
+++ omod/src/main/java/org/openmrs/module/radiology/web/extension/html/GutterListExt.java
@@ -0,0 +1,32 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.radiology.web.extension.html;
+
+import org.openmrs.module.radiology.RadiologyPrivileges;
+import org.openmrs.module.radiology.order.web.RadiologyOrderListController;
+import org.openmrs.module.web.extension.LinkExt;
+
+public class GutterListExt extends LinkExt {
+	
+	@Override
+	public String getLabel() {
+		return "radiology.home.title";
+	}
+	
+	@Override
+	public String getRequiredPrivilege() {
+		return RadiologyPrivileges.VIEW_RADIOLOGY_SECTION;
+	}
+	
+	@Override
+	public String getUrl() {
+		return RadiologyOrderListController.RADIOLOGY_ORDER_LIST_REQUEST_MAPPING;
+	}
+}
