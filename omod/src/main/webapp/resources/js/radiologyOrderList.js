f2bfe51e240b2e1303a5d6e587926b3b574d57fd
teleivo <mrteleivo@gmail.com>
2016-06-10 17:29:51 +0200

    make use of searchhandler

--- omod/src/main/webapp/resources/js/radiologyOrderList.js
+++ omod/src/main/webapp/resources/js/radiologyOrderList.js
@@ -10,72 +10,78 @@ $j(document)
                   results = $j('#results');
                   clearResults = $j('a#clearResults');
 
-                  firstTime();
-                  function firstTime() {
-                    sendRequest();
-                  }
-
-                  function sendRequest() {
-
-                    $j('#errorSpan').html('');
-                    var oTable = $j('#radiologyOrdersTable')
-                            .DataTable(
-                                    {
-                                      "processing": true,
-                                      "serverSide": true,
-                                      "ajax": {
-                                        headers: {
-                                          Accept: "application/json; charset=utf-8",
-                                          "Content-Type": "text/plain; charset=utf-8"
-                                        },
-                                        dataType: "json",
-                                        url: "http://localhost:8080/openmrs/ws/rest/v1/radiologyorder/63359e3b-2e40-411a-9449-139b26de839a",
-                                        "dataSrc": function(json) {
-                                          //for ( var i=0, ien=json.length ; i<ien ; i++ ) {
-                                          console.log(json);
-                                          //}
-                                          return [[
+                  $j('#errorSpan').html('');
+                  var radiologyOrdersTable = $j('#radiologyOrdersTable')
+                          .DataTable(
+                                  {
+                                    "processing": true,
+                                    "serverSide": true,
+                                    "ajax": {
+                                      headers: {
+                                        Accept: "application/json; charset=utf-8",
+                                        "Content-Type": "text/plain; charset=utf-8",
+                                      },
+                                      cache: true,
+                                      dataType: "json",
+                                      url: "http://localhost:8080/openmrs/ws/rest/v1/radiologyorder/",
+                                      data: function(data) {
+                                        console.log(data);
+                                        return {
+                                          startIndex: data.start,
+                                          limit: data.length,
+                                          v: "full",
+                                          q: "JUSTFORNOW",
+                                          patient: pQuery.val(),
+                                        };
+                                      },
+                                      "dataSrc": function(json) {
+                                        console.log(json);
+                                        var result = [];
+                                        for (var i = 0, ien = json.results.length; i < ien; i++) {
+                                          result[i] = [
                                               "OpenChildRow",
                                               '<a href="http://localhost:8080/openmrs/module/radiology/radiologyOrder.form?orderId='
-                                                      + json.uuid
+                                                      + json.results[i].uuid
                                                       + '">'
-                                                      + json.orderNumber
+                                                      + json.results[i].orderNumber
                                                       + '</a>',
-                                              json.patient.display,
-                                              json.urgency, json.dateActivated,
+                                              json.results[i].patient.display,
+                                              json.results[i].urgency,
+                                              json.results[i].dateActivated,
                                               "modality", "pstatus",
-                                              json.orderer.display, "sstatus",
-                                              json.instructions]];
+                                              json.results[i].orderer.display,
+                                              "sstatus",
+                                              json.results[i].instructions]
                                         }
+                                        return result;
+                                      }
+                                    },
+                                    "order": [[1, 'asc']],
+                                    "oLanguage": {
+                                      "sLengthMenu": '<spring:message code="radiology.show"/>'
+                                              + ' _MENU_ <spring:message code="radiology.entries"/>',
+                                      "sSearch": '<spring:message code="general.search"/>:',
+                                      "sInfo": '<spring:message code="radiology.viewing"/> _START_ '
+                                              + '- _END_ '
+                                              + '<spring:message code="radiology.of"/> _TOTAL_',
+                                      "oPaginate": {
+                                        "sFirst": '<spring:message code="radiology.first"/>',
+                                        "sPrevious": '<spring:message code="general.previous"/>',
+                                        "sNext": '<spring:message code="general.next"/>',
+                                        "sLast": '<spring:message code="radiology.last"/>',
                                       },
-                                      "order": [[1, 'asc']],
-                                      "oLanguage": {
-                                        "sLengthMenu": '<spring:message code="radiology.show"/>'
-                                                + ' _MENU_ <spring:message code="radiology.entries"/>',
-                                        "sSearch": '<spring:message code="general.search"/>:',
-                                        "sInfo": '<spring:message code="radiology.viewing"/> _START_ '
-                                                + '- _END_ '
-                                                + '<spring:message code="radiology.of"/> _TOTAL_',
-                                        "oPaginate": {
-                                          "sFirst": '<spring:message code="radiology.first"/>',
-                                          "sPrevious": '<spring:message code="general.previous"/>',
-                                          "sNext": '<spring:message code="general.next"/>',
-                                          "sLast": '<spring:message code="radiology.last"/>',
-                                        },
-                                        "sProcessing": '<spring:message code="general.loading"/>'
-                                      },
-                                    });
-                  }
+                                      "sProcessing": '<spring:message code="general.loading"/>'
+                                    },
+                                  });
 
                   // ***********Events*************
                   find.click(function() {
-                    page = 0;
-                    sendRequest();
+                    radiologyOrdersTable.ajax.reload();
                   });
 
                   pQuery.keypress(function(event) {
                     if (event.which == '13') {
-                      sendRequest();
+                      radiologyOrdersTable.ajax.reload();
                     }
                   });
 
